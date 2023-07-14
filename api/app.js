08b322a450bbd45f80c9982d3ac6502eb3b91be3
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-07-14 23:02:04 +0100

    crud quiz V1

--- api/app.js
+++ api/app.js
@@ -4,11 +4,13 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const config = require("./config");
 const courseroutes = require("./routers/course-routes");
+const quizreoutes = require("./routers/quizRoute");
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api", courseroutes.routes);
+app.use("/api", quizreoutes.routes);
 
 app.listen(config.port, () =>
   console.log(`App is listening on URI http://localhost:${config.port}`)
---
+++ api/models/evalution.js
@@ -0,0 +1,7 @@
+class Evalution{
+    constructor(id ,score){
+        this.id=id;
+        this.score=score;
+    }
+}
+module.exports=Evalution;
