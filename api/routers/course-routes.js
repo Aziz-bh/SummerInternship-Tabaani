df176454f207faa76fa4d782ad32ef6e1144f7fc
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-07-14 15:57:58 +0100

    updates

--- api/routers/course-routes.js
+++ api/routers/course-routes.js
@@ -1,17 +1,27 @@
-const express = require('express');
-const { addCourse,getAllcourses,getcourse,updatecourse ,deletecourse} = require('../Controllers/coursecontroller');
-const { addchapter, deletechapter, updatechapter} = require('../Controllers/chaptercontroller');
+const express = require("express");
+const {
+  addCourse,
+  getAllcourses,
+  getcourse,
+  updatecourse,
+  deletecourse,
+} = require("../Controllers/coursecontroller");
+const {
+  addchapter,
+  deletechapter,
+  updatechapter,
+} = require("../Controllers/chaptercontroller");
 const router = express.Router();
 
-router.post('/course', addCourse);
-router.get('/courses', getAllcourses);
-router.get('/course/:id',getcourse);
-router.put('/course/:id',updatecourse);
-router.delete('/course/:id',deletecourse);
-router.post('/course/:courseId/add-chapter', addchapter);
-router.delete('/course/:courseId/deletechapter/:chapterId',deletechapter);
-router.put('/course/:courseId/updatechapter/:chapterId',updatechapter);
+router.post("/course", addCourse);
+router.get("/courses", getAllcourses);
+router.get("/course/:id", getcourse);
+router.put("/course/:id", updatecourse);
+router.delete("/course/:id", deletecourse);
+router.post("/course/:courseId/add-chapter", addchapter);
+router.delete("/course/:courseId/deletechapter/:chapterId", deletechapter);
+router.put("/course/:courseId/updatechapter/:chapterId", updatechapter);
 
 module.exports = {
-  routes: router
+  routes: router,
 };
