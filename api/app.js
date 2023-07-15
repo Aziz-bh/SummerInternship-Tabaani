acb065dc155c1d26d296e17a9238b462a7a5e0fd
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-07-15 11:17:21 +0100

    a single route link

--- api/app.js
+++ api/app.js
@@ -1,17 +1,16 @@
 "use strict";
 const express = require("express");
 const cors = require("cors");
+const indexRouter = require("./routers/index");
 const bodyParser = require("body-parser");
 const config = require("./config");
-const courseroutes = require("./routers/course.router");
-const quizreoutes = require("./routers/quiz.router");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
 //the routes of all the project
-const indexRouter = require("./routers/index");
 app.use("/", indexRouter);
 
 app.listen(config.port, () =>
