d69bcd4dfc157eea0963bd8b27116f19e7192953
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-07-22 13:09:55 +0100

    updates ui

--- Client/src/views/admin/Courses/index.jsx
+++ Client/src/views/admin/Courses/index.jsx
@@ -55,18 +55,18 @@ const Courses = () => {
       image:
         "https://plus.unsplash.com/premium_photo-1661369931884-0706c99d88b3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1212&q=80",
     },
-    // Add more next courses as needed
   ];
 
   const sliderSettings = {
     dots: true,
     infinite: true,
+    arrows: false,
     speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 768, // Adjust this breakpoint value as needed
+        breakpoint: 768,
         settings: {
           slidesToShow: 1,
         },
