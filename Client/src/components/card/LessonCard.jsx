d5f9b1c7e00814d8bcff16474655f9db06229280
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-07-24 12:43:24 +0100

    course ui updates

--- Client/src/components/card/LessonCard.jsx
+++ Client/src/components/card/LessonCard.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 
 const LessonCard = () => {
+  const videoRef = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const togglePlay = () => {
+    const video = videoRef.current;
+
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+
+    setIsPlaying(!isPlaying);
+  };
   return (
     <div className="rounded-md bg-white p-4 shadow-md">
-      {/* Title */}
-      <h2 className="mb-4 text-2xl font-bold">Course Title</h2>
+      <h2 className="mb-4 text-2xl font-bold">
+        Course – Introduction to Hosting
+      </h2>
 
       {/* Video Container */}
-      <div className="mb-4 h-full w-full rounded-lg">
+      <div className="mb-4 h-full w-full overflow-hidden rounded-3xl border-2 border-gray-300 bg-gray-100">
         <ReactPlayer
           url="https://www.youtube.com/watch?v=Ades3pQbeh8"
-          controls={true}
+          type="video/webm"
+          controls={false}
           volume={0.6}
           muted={false}
           loop={false}
@@ -19,22 +35,21 @@ const LessonCard = () => {
         />
       </div>
 
-      {/* Avatar Picture and Title */}
       <div className="mb-4 flex items-center">
         <img
-          src="https://via.placeholder.com/40"
+          src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80"
           alt="Avatar"
-          className="mr-2 h-10 w-10 rounded-full"
+          className="mr-2 h-10 w-10 rounded-full object-cover"
         />
-        <p className="font-semibold">Instructor Name</p>
+        <p className="font-semibold">Lesson 1 – Implement Story telling</p>
       </div>
-
-      {/* Description */}
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor
-        dictum turpis ac interdum. Suspendisse potenti. Fusce at dolor euismod,
-        euismod justo quis, ullamcorper nisi. In vel magna vitae turpis
-        facilisis scelerisque.
+        This module would provide strategies for providing exceptional customer
+        service and creating a welcoming and comfortable environment for
+        visitors. It would cover topics such as communication skills,
+        problem-solving, and attention to detail. Participants would learn how
+        to anticipate and respond to the needs of visitors, and how to create a
+        hospitable and inclusive environment for all.
       </p>
     </div>
   );
