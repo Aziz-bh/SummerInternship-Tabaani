9e2e307f983ae9fbc635e1583d16f8611e6af980
hadil-msadak <96638211+hadil-msadak@users.noreply.github.com>
2023-08-09 08:15:33 +0100

    dash

--- Client/src/components/AdminSidebar/index.jsx
+++ Client/src/components/AdminSidebar/index.jsx
@@ -4,6 +4,7 @@ import { HiX } from "react-icons/hi";
 import Links from "./components/Links";
 
 import routes from "routes.js";
+import Vector from "assets/img/Logos/TabaaniAcademyLogo.png"
 
 const AdminSidebar = ({ open, onClose }) => {
   return (
@@ -18,11 +19,13 @@ const AdminSidebar = ({ open, onClose }) => {
       >
         <HiX />
       </span>
-
+      <div className="mt-9 flex items-center space-x-1 font-poppins text-[20px] font-bold uppercase dark:text-white">
+          <img src={Vector} alt="Tabaani Academy Logo" />
+        </div>
       <div className={`mx-[40px] mt-[50px] flex items-center`}>
         <div className="mt-1 flex items-center space-x-1 font-poppins text-[24px] font-bold uppercase dark:text-white">
+       
           <span className="mt-5 font-medium text-white">Admin</span>
-
           <span className="mt-5 font-medium text-white">Dashboard</span>
         </div>
       </div>
