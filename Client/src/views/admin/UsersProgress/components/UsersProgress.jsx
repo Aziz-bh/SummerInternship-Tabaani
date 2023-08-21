96f92a23f816a117f2350128ca9f72dd21c7dc4d
Mouhib Touaiti <mouhib.touaiti@esprit.tn>
2023-08-21 15:35:06 +0100

    updates

--- Client/src/views/admin/UsersProgress/components/UsersProgress.jsx
+++ Client/src/views/admin/UsersProgress/components/UsersProgress.jsx
@@ -1,13 +1,12 @@
-
 import Card from "components/card";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   useGlobalFilter,
   usePagination,
   useSortBy,
   useTable,
-} from 'react-table';
+} from "react-table";
 import { MdCheckCircle, MdCancel } from "react-icons/md";
 import Progress from "components/progress";
 const ComplexTable = () => {
@@ -16,27 +15,29 @@ const ComplexTable = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/users'); // Replace USER_ID with the actual user ID
+        const response = await axios.get("http://localhost:5000/api/users"); // Replace USER_ID with the actual user ID
         const responseData = response.data;
         console.log(responseData.usersWithCourses);
         // Check if the response has usersWithCourses array
         if (Array.isArray(responseData.usersWithCourses)) {
-          const formattedData = responseData.usersWithCourses.map(user => ({
-            picture: user.userData.ProfilePic,
-            name: user.userData.firstname, // Replace with the actual user property
+          const formattedData = responseData.usersWithCourses.map((user) => ({
+            picture: user.userData.photoURL,
+            name: user.userData.displayName, // Replace with the actual user property
             status: user.userData.verified ? "Verified" : "Unverified",
             country: user.userData.country,
-            courses: user.subscribedCourses.map(course => course.title),
-            progress: user.subscriptions.map(subscription => subscription.progress),
+            courses: user.subscribedCourses.map((course) => course.title),
+            progress: user.subscriptions.map(
+              (subscription) => subscription.progress
+            ),
             // Replace with the actual progress property
           }));
-          console.log("hh" + formattedData)
+          console.log("hh" + formattedData);
           setTableData(formattedData);
         } else {
-          console.error('Invalid API response:', responseData);
+          console.error("Invalid API response:", responseData);
         }
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -46,30 +47,29 @@ const ComplexTable = () => {
   const columns = React.useMemo(
     () => [
       {
-        Header: '',
-        accessor: 'picture', // Replace with the correct accessor for progress
+        Header: "",
+        accessor: "picture", // Replace with the correct accessor for progress
       },
       {
-        Header: 'NAME',
-        accessor: 'name',
+        Header: "NAME",
+        accessor: "name",
       },
       {
-        Header: 'STATUS',
-        accessor: 'status',
+        Header: "STATUS",
+        accessor: "status",
       },
       {
-        Header: 'COUNTRY',
-        accessor: 'country', // Replace with the correct accessor for date
+        Header: "COUNTRY",
+        accessor: "country", // Replace with the correct accessor for date
       },
       {
-        Header: 'COURSES',
-        accessor: 'courses', // Replace with the correct accessor for date
+        Header: "COURSES",
+        accessor: "courses", // Replace with the correct accessor for date
       },
       {
-        Header: 'PROGRESS',
-        accessor: 'progress', // Replace with the correct accessor for progress
+        Header: "PROGRESS",
+        accessor: "progress", // Replace with the correct accessor for progress
       },
-
     ],
     []
   );
@@ -96,7 +96,6 @@ const ComplexTable = () => {
     <Card extra={"w-full h-full p-4 sm:overflow-x-auto"}>
       <div class="relative flex items-center justify-between">
         <div class="text-xl font-bold  dark:text-white">Users Progress</div>
-
       </div>
 
       <div class="mt-8 h-full overflow-x-scroll xl:overflow-hidden">
@@ -108,7 +107,7 @@ const ComplexTable = () => {
                   <th
                     {...column.getHeaderProps(column.getSortByToggleProps())}
                     key={index}
-                    className="border-b border-gray-200 pr-28 pb-[10px] text-start dark:!border-navy-700"
+                    className="border-b border-gray-200 pb-[10px] pr-28 text-start dark:!border-navy-700"
                   >
                     <p className="text-xs tracking-wide text-gray-600">
                       {column.render("Header")}
@@ -133,20 +132,16 @@ const ComplexTable = () => {
                             <img
                               className="h-full w-full rounded-full"
                               src={cell.value}
-
                             />
-
                           </div>
                         </div>
                       );
-                    }
-                    else if (cell.column.Header === "NAME") {
+                    } else if (cell.column.Header === "NAME") {
                       data = (
                         <p className="text-sm font-bold text-navy-700 dark:text-white">
                           {cell.value}
                         </p>
                       );
-
                     } else if (cell.column.Header === "STATUS") {
                       data = (
                         <div className="flex items-center gap-2">
@@ -172,18 +167,23 @@ const ComplexTable = () => {
                       data = (
                         <div className="flex flex-col">
                           {cell.value.map((course, index) => (
-                            <div key={index} className="flex items-center gap-2 ">
+                            <div
+                              key={index}
+                              className="flex items-center gap-2 "
+                            >
                               <p className="font-bold">{course}</p>
                             </div>
                           ))}
                         </div>
                       );
-                    }
-                    else if (cell.column.Header === "PROGRESS") {
+                    } else if (cell.column.Header === "PROGRESS") {
                       data = (
                         <div className="flex flex-col">
                           {cell.value.map((progress, index) => (
-                            <div key={index} className="flex items-center gap-2">
+                            <div
+                              key={index}
+                              className="flex items-center gap-2"
+                            >
                               <Progress width="w-48" value={progress} />
                               <p className="font-bold">{progress}%</p>
                             </div>
@@ -193,7 +193,7 @@ const ComplexTable = () => {
                     }
                     return (
                       <td
-                        className="pt-[14px] pb-[18px] sm:text-[14px]"
+                        className="pb-[18px] pt-[14px] sm:text-[14px]"
                         {...cell.getCellProps()}
                         key={index}
                       >
